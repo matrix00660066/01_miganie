549cde7a1ed98828e20f9d7a63e571aea1b1b9ea
matrix00660066 <matrix0606@wp.pl>
2023-01-10 20:26:18 +0100

    kolejne testy programu z miganiem i lcd

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,24 @@
 */
 
 #include <Arduino.h>
-
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 int ledPin = 13;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
   pinMode(ledPin, OUTPUT);
+  lcd.init();
+  lcd.clear();
+  delay(250);
+  lcd.backlight();
+  Serial.begin(9600);
+  lcd.setCursor(0, 0);
+  lcd.print(" Start programu ");
+  delay(500);
+  lcd.clear();
 }
 
 void wlacznik(); // deklaracja funkcji włącznik
@@ -16,11 +28,30 @@ void wlacznik(); // deklaracja funkcji włącznik
 void loop()
 {
   digitalWrite(ledPin, 1);
-  delay(1000);
+  delay(50);
   digitalWrite(ledPin, 0);
-  delay(1500);
+  delay(50);
+  digitalWrite(ledPin, 1);
+  delay(50);
+  digitalWrite(ledPin, 0);
+  delay(50);
+  digitalWrite(ledPin, 1);
+  delay(50);
+  digitalWrite(ledPin, 0);
+  delay(800);
+  lcd.setCursor(0, 0);
+  lcd.print("Hallo");
+  delay(500);
+  lcd.setCursor(0, 0);
+  lcd.print("                ");
+  delay(2500);
+  wlacznik();
 }
 
 void wlacznik() // ta prosta funkjca na razie nie robi nic
 {               // to tylko test czy kompilator nie wywala błędów
+  lcd.setCursor(0, 0);
+  lcd.print("Funkcja wlacznik");
+  delay(10000);
+  lcd.clear();
 }
