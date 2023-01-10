177611e7a4c64b96a7f73e61af65951cd35b6da1
matrix00660066 <matrix0606@wp.pl>
2023-01-10 20:58:24 +0100

    czyszczenie lcd przed jego inicjalizacją

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/*prosty program, którego nawet nie da się nazwać programem. Miga co sekundę wbudowaną diodą led
+/*prosty program, którego nawet nie da się nazwać programem. Miga wbudowaną diodą led
         https://github.com/matrix00660066/01_miganie
 */
 
@@ -14,13 +14,9 @@ void setup()
   pinMode(ledPin, OUTPUT);
   lcd.init();
   lcd.clear();
-  delay(250);
+  delay(10);
   lcd.backlight();
   Serial.begin(9600);
-  lcd.setCursor(0, 0);
-  lcd.print(" Start programu ");
-  delay(500);
-  lcd.clear();
 }
 
 void wlacznik(); // deklaracja funkcji włącznik
@@ -48,10 +44,6 @@ void loop()
   wlacznik();
 }
 
-void wlacznik() // ta prosta funkjca na razie nie robi nic
-{               // to tylko test czy kompilator nie wywala błędów
-  lcd.setCursor(0, 0);
-  lcd.print("Funkcja wlacznik");
-  delay(10000);
-  lcd.clear();
+void wlacznik() // ta prosta funkjca na razie nie robi nic, to tylko test czy kompilator nie wywala błędów wywołania funkcji
+{
 }
